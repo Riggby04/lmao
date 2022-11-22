eaf259083624b029e8cda45f9071fe0a772d3d50
Wouter van Reeven <info@wwwappz.nl>
2022-11-22 12:36:18 +0100

    fiutmelding

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           ik ben Ole en dit is mijn website 
-        </h1>
+        </h1
 
         <p className={styles.description}>
          
